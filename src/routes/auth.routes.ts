17db26a98df419b142a312d94efa74c7c2fb168c
codingwithcod <codingwithcod@gmail.com>
2023-11-05 10:54:36 +0530

    Add validator middleware funciton

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -5,12 +5,27 @@ import {
   login,
   signup,
 } from "../controllers/auth.controller";
+import validator from "../validator";
+import {
+  forgetPasswordEmailInputSchema,
+  forgetPasswordInputSchema,
+  loginInputSchema,
+  signupInputSchema,
+} from "../validator/authSchema";
 
 const authRouter = Router();
 
-authRouter.post("/signup", signup);
-authRouter.post("/login", login);
-authRouter.post("/forget-password-email", handleForgetPasswordEmail);
-authRouter.post("/forget-password", handleForgetPassword);
+authRouter.post("/signup", validator(signupInputSchema), signup);
+authRouter.post("/login", validator(loginInputSchema), login);
+authRouter.post(
+  "/forget-password-email",
+  validator(forgetPasswordEmailInputSchema),
+  handleForgetPasswordEmail
+);
+authRouter.post(
+  "/forget-password",
+  validator(forgetPasswordInputSchema),
+  handleForgetPassword
+);
 
 export default authRouter;
